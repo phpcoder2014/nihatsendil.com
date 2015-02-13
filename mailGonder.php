43bd466e7013c49b4fd29c5a44a0e18fc304c9d3
Nihat <nihatsendil@gmail.com>
2015-02-13 14:53:07 +0200

    güncel
    güncel

--- mailGonder.php
+++ mailGonder.php
@@ -5,10 +5,10 @@
 <?php
 //ob_start();
 require_once("PHPMailerAutoload.php");
-$name					= $_POST['contact_name'];
-$email					= $_POST['contact_email'];
-$message				= $_POST['contact_message'];
-$phone					= $_POST['contact_phone'];
+$name				= $_POST['contact_name'];
+$email				= $_POST['contact_email'];
+$message			= $_POST['contact_message'];
+$phone				= $_POST['contact_phone'];
 $mail = new PHPMailer();
 $mail->IsSMTP();
 $mail->SMTPDebug 		 = 0;
@@ -16,15 +16,15 @@ $mail->SMTPAuth 		 = true;
 $mail->Host 			 = "smtp.gmail.com";
 $mail->SMTPSecure 		 = 'ssl';
 $mail->CharSet 			 = 'UTF-8';
-$mail->Port				 = 465;
-$mail->Username			 = "nihatsendil@gmail.com";
-$mail->Password 		 = "google3252439";
+$mail->Port			 = 465;
+$mail->Username			 = "deneme@gmail.com";
+$mail->Password 		 = "password";
 $mail->SetFrom($name);
 $mail->Subject			 = $name;
 $body 	     			 = $message;
 $mail->MsgHTML($body . "<br/><br/><br/>E-posta &nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;". $email . "<br/>Adı Soyadı :&nbsp;" . $name . "<br/>Telefon &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;". $phone);
 // hedef adresi ekle
-$to = 'nihatsendil@gmail.com';
+$to = 'deneme@gmail.com';
 
 $mail->AddAddress($to, "Yeni Bir Mesaj Var.!");
 // Maili gönder
